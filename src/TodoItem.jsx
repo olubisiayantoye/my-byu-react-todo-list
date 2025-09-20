f62c60b11d4d6bf6c102d67b4877e1e91e17211a
olubisiayantoye <bisikingson@gmail.com>
2025-09-20 18:11:23 +0100

    modified:   src/TodoItem.jsx 	modified:   src/TodoList.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,15 +1,23 @@
+// Component for displaying a single ToDo item
 export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
   return (
     <li>
       <label>
+        {/* Checkbox for marking todo as completed */}
         <input
           type="checkbox"
-          checked={completed}
-          onChange={e => toggleTodo(id, e.target.checked)}
+          checked={completed} // Checked state is controlled by prop
+          onChange={e => toggleTodo(id, e.target.checked)} // Toggle completed state
         />
+        {/* Display todo title */}
         {title}
       </label>
-      <button onClick={() => deleteTodo(id)} className="btn btn-danger">
+
+      {/* Delete button to remove this todo */}
+      <button 
+        onClick={() => deleteTodo(id)} 
+        className="btn btn-danger"
+      >
         Delete
       </button>
     </li>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,16 +1,20 @@
 import { TodoItem } from "./TodoItem"
 
+// Component for rendering the list of all ToDo items
 export function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
     <ul className="list">
+      {/* Show message if there are no todos */}
       {todos.length === 0 && "No Todos"}
+
+      {/* Map through todos array and render each TodoItem */}
       {todos.map(todo => {
         return (
           <TodoItem
-            {...todo}
-            key={todo.id}
-            toggleTodo={toggleTodo}
-            deleteTodo={deleteTodo}
+            {...todo}              // Spread todo props: id, title, completed
+            key={todo.id}          // Unique key for React list rendering
+            toggleTodo={toggleTodo} // Function to toggle completion
+            deleteTodo={deleteTodo} // Function to delete todo
           />
         )
       })}
